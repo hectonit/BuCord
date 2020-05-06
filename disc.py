138e8bc7cac8a38d9b559b3c38b5cf129e0f7649
Phantistic <48787141+Phantistic@users.noreply.github.com>
2020-05-06 13:02:42 +0300

    Update disc.py

--- disc.py
+++ disc.py
@@ -9,24 +9,20 @@ client = discord.Client()
 
 finance = {}
 profile = {}
-profilearr = [0, 0]
 
 
-
-@bot.event
-async def on_message(message):
-    if profile.get(message.author.id) == None:
-        profile[message.author.id] = profilearr
+@client.event
+async def on_message(ctx):
+    if profile.get(ctx.author.id) == None:
+        profile[ctx.author.id] = [0, 0]
     else:
-        profile[message.author.id][0] += 1
-        if profile[message.author.id][0] >= 100:
-            profile[message.author.id][1] += 1
-            profile[message.author.id][0] = 0
-    if finance.get(message.author.id) == None:
-        finance[message.author.id] = 2
+        profile[ctx.author.id][0] += 1
+        if profile[ctx.author.id][0] >= 100:
+            profile[ctx.author.id][1] += 1
+    if finance.get(ctx.author.id) == None:
+        finance[ctx.author.id] = 2
     else:
-        finance[message.author.id] += 1
-    await bot.process_commands(message)
+        finance[ctx.author.id] += 1
 
 
 @bot.command()
@@ -37,7 +33,7 @@ async def test(ctx, arg):
 @bot.command()
 async def stavka(ctx, arg):
     if profile.get(ctx.author.id) == None:
-        profile[ctx.author.id] = profilearr
+        profile[ctx.author.id] = [0, 0]
     arg = int(arg)
     if arg > finance[ctx.author.id] or arg <= 0:
         await ctx.send("Число введено неверно.")
@@ -59,11 +55,36 @@ async def balans(ctx):
 @bot.command()
 async def profiler(ctx):
     if profile.get(ctx.author.id) == None:
-        profile[ctx.author.id] = profilearr
+        profile[ctx.author.id] = [0, 0]
+    print(ctx.author.id)
     await ctx.send(
         "@{}, ваши очки: {}, уровень: {}".format(ctx.author, profile[ctx.author.id][0],
                                                  profile[ctx.author.id][1]))
 
 
+@bot.command()
+async def top(ctx):
+    torr = list(finance.values())
+    torr.sort()
+    torr = torr[len(torr) - 5:]
+    ind1 = 0
+    ind2 = 0
+    ind3 = 0
+    ind4 = 0
+    ind5 = 0
+    for k, v in finance.items():
+        if v in torr and torr.index(v) == 0:
+            ind1 = ("Первое место - {}".format(bot.get_user(k)))
+        elif v in torr and torr.index(v) == 1:
+            ind2 = ("Второе место - {}".format(bot.get_user(k)))
+        elif v in torr and torr.index(v) == 2:
+            ind3 = ("Третье место - {}".format(bot.get_user(k)))
+        elif v in torr and torr.index(v) == 3:
+            ind4 = ("Четвертое место - {}".format(bot.get_user(k)))
+        elif v in torr and torr.index(v) == 4:
+            ind5 = ("Пятое место - {}".format(bot.get_user(k)))
+    await ctx.send("{}\n{}\n{}\n{}\n{}".format(ind1, ind2, ind3, ind4, ind5))
+
+
 token = os.environ.get('BOT_TOKEN')
-bot.run(str(token))
+bot.run('token')
