b2464a7092c0b56ce4e6762fa3f05262c8c6d634
hectonit <progcuber@gmail.com>
2021-08-15 12:18:26 +0300

    refactor main.py

--- main.py
+++ main.py
@@ -4,30 +4,18 @@ import dbl
 import discord
 from discord.ext import commands
 
-import configs
-import utils
 from configs.constants import BOT_TOKEN, DBL_TOKEN
-from utils.db import *
+from utils.db import get_guild_info
 
 
 async def dynamic_prefix(pref_bot, message):
-    """
-    function for dynamic prefix for bot
-
-    :param pref_bot: discord bot
-    :type pref_bot: commands.Bot
-    :param message: each message
-    :type message: discord.Message
-    :return: prefix
-    :rtype: str
-    """
     guild = message.guild
     if guild is None:
         return await pref_bot.get_prefix(message)
-    else:
-        guild_info = await get_guild_info(guild.id)
-        prefix = guild_info["prefix"]
-        return commands.when_mentioned_or(prefix)(pref_bot, message)
+
+    guild_info = await get_guild_info(guild.id)
+    prefix = guild_info["prefix"]
+    return commands.when_mentioned_or(prefix)(pref_bot, message)
 
 
 intents = discord.Intents.all()
